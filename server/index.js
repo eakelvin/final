471c34a60991670ea417b26eea9fce7d8a3ac9ce
Kelvin Akaba <akabakelvin@gmail.com>
2023-08-23 17:27:13 +0000

    readline + ai

--- server/index.js
+++ server/index.js
@@ -1,35 +1,47 @@
-import OpenAI from 'openai';
-import dotenv from 'dotenv'
-dotenv.config()
-
-const openai = new OpenAI({
-  apiKey: process.env.API_KEY
-});
-
-// const openai = require("openai")
-// const dotenv = require("dotenv")
-// dotenv.config()
-// const apiKey = process.env.API_KEY
-// openai.apiKey = apiKey
+import openai from "./config/open-ai.js";
+import colors from 'colors'
+import readlineSync from 'readline-sync'
 
 async function main() {
+    console.log(colors.bold.green('Welcome to EA-CHAT'));
+    console.log(colors.bold.blue("Let's get started"));
 
-    const messages = [
-        {role: 'system', content: 'You are a rude assistant.'},
-        {role: 'user', content: 'Who created Facebook?'},
-        {role: 'assistant', content: 'Bitch do not be lazy, do some research.'}
-    ]
+    const chatHistory = []
 
-    try {
-        const response = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
-            messages : messages
-        })
-        console.log(response);
-    } catch (error) {
-        console.error("Error:", error);
+    while (true) {
+        const userInput = readlineSync.question(colors.yellow('YOU: '))
+        // console.log(`Hello ${userInput}`);
+
+        const messages = [
+            {role: 'system', content: 'You are a rude assistant.'},
+            {role: 'user', content: userInput},
+            {role: 'assistant', content: 'Bitch do not be lazy, do some research.'}
+        ]
+
+        try {
+            const messages = chatHistory.map(([role, content]) => ({ role, content }))
+            messages.push({ role: 'user', content: userInput})
+            const response = await openai.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages : messages
+            })
+
+            const reply = response.choices
+
+            if (userInput.toLowerCase() === 'exit'){
+                console.log(colors.green('BOT: ') + reply);
+                return;
+            }
+
+            console.log(colors.green('BOT: ') + reply);
+            
+            chatHistory.push['user', userInput]
+            chatHistory.push['assistant', response]
+        } catch (error) {
+            console.error("Error:", colors.red(error));
+        }
     }
-    
+
 }
 
 main()
